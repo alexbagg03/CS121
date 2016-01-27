e5e79ed058032df515147dff7293954134cdc24c
Alexander Bagg <alexbagg03@gmail.com>
2016-01-26 20:22:31 -0800

    Initial commit of Assignment 2 folder

---
+++ Assignment2/src/ir/assignments/two/Controller.java
@@ -0,0 +1,74 @@
+package ir.assignments.two;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+/**
+ * Controller class from library: https://github.com/yasserg/crawler4j.git
+ *
+ * Created for CS121 Assignment 2 by:
+ * Alex Bagg (80332851)
+ */
+public class Controller {
+
+
+    ///////////////////////////////////////////
+    // MEMBERS
+    ///////////////////////////////////////////
+    private static final String crawlStorageFolder = "/Users/Alex/Documents/IntelliJ/CS121/Assignment2/data/crawl/root";
+    private static final int numberOfCrawlers = 7;
+    private static CrawlController mCrawlController;
+    private static final String userAgentString = "UCI Inf141-CS121 crawler 80332851";
+
+
+    ///////////////////////////////////////////
+    // CONSTRUCTOR
+    ///////////////////////////////////////////
+    public Controller(String seed){
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+        config.setPolitenessDelay(600);
+        config.setUserAgentString(userAgentString);
+        config.setResumableCrawling(true);
+        config.setMaxDepthOfCrawling(3);
+
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+
+        try {
+            mCrawlController = new CrawlController(config, pageFetcher, robotstxtServer);
+            mCrawlController.addSeed(seed);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+
+    ///////////////////////////////////////////
+    // METHODS
+    ///////////////////////////////////////////
+    public static void start(){
+        /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+        mCrawlController.start(Crawler.class, numberOfCrawlers);
+
+    }
+
+}
---
+++ Assignment2/src/ir/assignments/two/Crawler.java
@@ -0,0 +1,102 @@
+package ir.assignments.two;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class Crawler extends WebCrawler {
+
+	///////////////////////////////////////////
+	// MEMBERS
+	///////////////////////////////////////////
+	private static ArrayList<String> urlCollection;
+
+
+	/**
+	 * This method is for testing purposes only. It does not need to be used
+	 * to answer any of the questions in the assignment. However, it must
+	 * function as specified so that your crawler can be verified programatically.
+	 * 
+	 * This methods performs a crawl starting at the specified seed URL. Returns a
+	 * collection containing all URLs visited during the crawl.
+	 */
+	public static Collection<String> crawl(String seedURL) {
+		Controller controller = new Controller(seedURL);
+		urlCollection = new ArrayList<String>();
+		controller.start();
+
+		return urlCollection;
+
+	}
+
+	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg"
+			+ "|png|mp3|mp3|zip|gz))$");
+
+	/**
+	 * This method receives two parameters. The first parameter is the page
+	 * in which we have discovered this new url and the second parameter is
+	 * the new url. You should implement this function to specify whether
+	 * the given url should be crawled or not (based on your crawling logic).
+	 * In this example, we are instructing the crawler to ignore urls that
+	 * have css, js, git, ... extensions and to only accept urls that start
+	 * with "http://www.ics.uci.edu/". In this case, we didn't need the
+	 * referringPage parameter to make the decision.
+	 */
+	@Override
+	public boolean shouldVisit(Page referringPage, WebURL url) {
+		CrawlConfig config = new CrawlConfig();
+		PageFetcher pageFetcher = new PageFetcher(config);
+		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+
+		if(!robotstxtServer.allows(url)){
+			return false;
+
+		}
+
+		String href = url.getURL().toLowerCase();
+		return !FILTERS.matcher(href).matches()
+				&& href.startsWith("http://www.ics.uci.edu/");
+	}
+
+	/**
+	 * This function is called when a page is fetched and ready
+	 * to be processed by your program.
+	 */
+	@Override
+	public void visit(Page page) {
+		String url = page.getWebURL().getURL();
+		System.out.println("URL: " + url);
+		urlCollection.add(url);
+
+		if (page.getParseData() instanceof HtmlParseData) {
+			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+			String text = htmlParseData.getText();
+			String html = htmlParseData.getHtml();
+			Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+			System.out.println("Text length: " + text.length());
+			System.out.println("Html length: " + html.length());
+			System.out.println("Number of outgoing links: " + links.size());
+		}
+	}
+
+	///////////////////////////////////////////
+	// MAIN FUNCTION
+	///////////////////////////////////////////
+	public static void main(String[] args){
+		crawl("http://www.ics.uci.edu");
+
+	}
+
+}
