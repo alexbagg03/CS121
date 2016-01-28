bd85d7bc1e94e4cd949dee010e78856f64000d1b
Alexander Bagg <alexbagg03@gmail.com>
2016-01-28 12:49:19 -0800

    Added comments, started Subdomains.txt part
    I’ve implemented writing subdomains and the number of their outgoing
    links to the Subdomains.txt, just need to implement sorting the file
    once it’s done

--- Assignment2/src/ir/assignments/two/Controller.java
+++ Assignment2/src/ir/assignments/two/Controller.java
@@ -11,45 +11,56 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
  *
  * Created for CS121 Assignment 2 by:
  * Alex Bagg (80332851)
+ * <---------------------------------------- ADD YOUR NAME / ID HERE
  */
 public class Controller {
-
 
     ///////////////////////////////////////////
     // MEMBERS
     ///////////////////////////////////////////
-    private static final String crawlStorageFolder = "../data/crawl/root";
+    private static final String crawlStorageFolder = "../Assignment2/data/crawl/root";
     private static final int numberOfCrawlers = 7;
-    private static CrawlController mCrawlController;
+    private static final int politenessDelay = 600; // in milliseconds
+    private static final int maxDepthOfCrawling = 3;
+    private static CrawlController crawlController;
     private static final String userAgentString = "UCI Inf141-CS121 crawler 80332851 19367502";
 
 
     ///////////////////////////////////////////
     // CONSTRUCTOR
     ///////////////////////////////////////////
+    /**
+     * Constructor for the Controller that takes a seed url
+     * to begin crawling with. The crawler configurations are then
+     * set with a storage folder, politeness delay, user agent string,
+     * and whether resumable crawling is enabled/disabled. An instance
+     * of the CrawlController is then created and the given seed url
+     * is added to it.
+     *
+     * @param seed - Initial seed url to begin crawling on
+     */
     public Controller(String seed){
-        /*
-         * For each crawl, you need to add some seed urls. These are the first
-         * URLs that are fetched and then the crawler starts following links
-         * which are found in these pages
-         */
         CrawlConfig config = new CrawlConfig();
         config.setCrawlStorageFolder(crawlStorageFolder);
-        config.setPolitenessDelay(600);
+        config.setPolitenessDelay(politenessDelay);
         config.setUserAgentString(userAgentString);
-        config.setResumableCrawling(true);
-        config.setMaxDepthOfCrawling(3);
+        //TODO remove when done testing
+        config.setMaxDepthOfCrawling(maxDepthOfCrawling);
+        config.setResumableCrawling(false);
 
-        /*
-         * Instantiate the controller for this crawl.
-         */
         PageFetcher pageFetcher = new PageFetcher(config);
         RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
         RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
 
         try {
-            mCrawlController = new CrawlController(config, pageFetcher, robotstxtServer);
-            mCrawlController.addSeed(seed);
+            crawlController = new CrawlController(config, pageFetcher, robotstxtServer);
+
+            /*
+             * For each crawl, you need to add some seed urls. These are the first
+             * URLs that are fetched and then the crawler starts following links
+             * which are found in these pages
+             */
+            crawlController.addSeed(seed);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -62,12 +73,13 @@ public class Controller {
     ///////////////////////////////////////////
     // METHODS
     ///////////////////////////////////////////
+    /**
+     * Starts the crawl with the specific Crawler class and number of crawlers set.
+     * This is a blocking operation, meaning that your code will reach the line after
+     * this only when crawling is finished.
+     */
     public static void start(){
-        /*
-         * Start the crawl. This is a blocking operation, meaning that your code
-         * will reach the line after this only when crawling is finished.
-         */
-        mCrawlController.start(Crawler.class, numberOfCrawlers);
+        crawlController.start(Crawler.class, numberOfCrawlers);
 
     }
 
--- Assignment2/src/ir/assignments/two/Crawler.java
+++ Assignment2/src/ir/assignments/two/Crawler.java
@@ -9,9 +9,10 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 import edu.uci.ics.crawler4j.url.WebURL;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Set;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class Crawler extends WebCrawler {
@@ -19,38 +20,39 @@ public class Crawler extends WebCrawler {
 	///////////////////////////////////////////
 	// MEMBERS
 	///////////////////////////////////////////
+	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg" + "|png|mp3|mp3|zip|gz))$");
+	private final static String icsDomain = "http://www.ics.uci.edu";
+	private static final String subdomainFileDir = "../Assignment2";
+	private static final String subdomainFileName = "Subdomains.txt";
+	private static File subdomainFile;
 	private static ArrayList<String> urlCollection;
 
-
 	/**
-	 * This method is for testing purposes only. It does not need to be used
-	 * to answer any of the questions in the assignment. However, it must
-	 * function as specified so that your crawler can be verified programatically.
-	 * 
 	 * This methods performs a crawl starting at the specified seed URL. Returns a
 	 * collection containing all URLs visited during the crawl.
+	 *
+	 * @param seedURL - Seed URL to crawl
 	 */
 	public static Collection<String> crawl(String seedURL) {
 		Controller controller = new Controller(seedURL);
 		urlCollection = new ArrayList<String>();
+		createSubdomainFile();
+
 		controller.start();
 
 		return urlCollection;
 
 	}
 
-	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg"
-			+ "|png|mp3|mp3|zip|gz))$");
-
 	/**
-	 * This method receives two parameters. The first parameter is the page
-	 * in which we have discovered this new url and the second parameter is
-	 * the new url. You should implement this function to specify whether
-	 * the given url should be crawled or not (based on your crawling logic).
-	 * In this example, we are instructing the crawler to ignore urls that
-	 * have css, js, git, ... extensions and to only accept urls that start
-	 * with "http://www.ics.uci.edu/". In this case, we didn't need the
-	 * referringPage parameter to make the decision.
+	 * Specifies whether the given url should be crawled or not (based on your crawling logic).
+	 * It sets up a RobotstxtConfig and RobotstxtServer to check whether the given URL is
+	 * allowed to be visited.
+	 * The crawler will ignore urls that have css, js, git, ... extensions and will only
+	 * accept urls that start with "http://www.ics.uci.edu/".
+	 *
+	 * @param referringPage - Page referring to the given URL
+	 * @param url - URL to be checked whether to visit or not
 	 */
 	@Override
 	public boolean shouldVisit(Page referringPage, WebURL url) {
@@ -59,19 +61,23 @@ public class Crawler extends WebCrawler {
 		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
 		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
 
-		if(!robotstxtServer.allows(url)){
+		// If the robots.txt configuration is enabled and DOES NOT allow the given url
+		// DO NOT visit it
+		if(robotstxtConfig.isEnabled() && !robotstxtServer.allows(url)){
 			return false;
-
 		}
 
 		String href = url.getURL().toLowerCase();
-		return !FILTERS.matcher(href).matches()
-				&& href.startsWith("http://www.ics.uci.edu/");
+		return !FILTERS.matcher(href).matches() && href.startsWith(icsDomain);
 	}
 
 	/**
 	 * This function is called when a page is fetched and ready
-	 * to be processed by your program.
+	 * to be processed. It gathers the data, text, html, and outgoing
+	 * links of the page, then adds the URL as a key to the subdomainMap
+	 * with the number of outgoing links as a value;
+	 *
+	 * @param page - Page being visited on a specific URL
 	 */
 	@Override
 	public void visit(Page page) {
@@ -88,14 +94,78 @@ public class Crawler extends WebCrawler {
 			System.out.println("Text length: " + text.length());
 			System.out.println("Html length: " + html.length());
 			System.out.println("Number of outgoing links: " + links.size());
+
+			if(!url.equals(icsDomain + "/")) {
+				addSubdomain(url, links.size());
+
+			}
+
 		}
+
 	}
 
+	/**
+	 * Main function for running the crawler on the ICS domain.
+	 * This is primarily used for testing.
+	 *
+	 * @param args
+     */
 	///////////////////////////////////////////
 	// MAIN FUNCTION
 	///////////////////////////////////////////
 	public static void main(String[] args){
-		crawl("http://www.ics.uci.edu");
+		crawl(icsDomain);
+
+	}
+
+
+	///////////////////////////////////////////
+	// METHODS
+	///////////////////////////////////////////
+	private static void createSubdomainFile(){
+		subdomainFile = new File(subdomainFileDir, subdomainFileName);
+		BufferedWriter writer = null;
+
+		try {
+			writer = new BufferedWriter(new FileWriter(subdomainFile));
+			writer.write("");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				writer.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			}
+
+		}
+
+	}
+	private static void addSubdomain(String url, int nLinks){
+		BufferedWriter writer = null;
+
+		try {
+			writer = new BufferedWriter(new FileWriter(subdomainFile, true));
+			writer.write(url + ", " + nLinks);
+			writer.newLine();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				writer.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			}
+
+		}
 
 	}
 
