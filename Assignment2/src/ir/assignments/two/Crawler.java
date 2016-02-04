6bdb0c1996ff56655af82a715a48856370c6c501
Alexander Bagg <alexbagg03@gmail.com>
2016-02-03 21:37:22 -0800

    Revert "Revert "Merge branch 'master' of https://github.com/holmes1309/CS121""
    This reverts commit f27fe90f8310738cfca4405e4b504edbf76e68b4.

--- Assignment2/src/ir/assignments/two/Crawler.java
+++ Assignment2/src/ir/assignments/two/Crawler.java
@@ -9,9 +9,7 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 import edu.uci.ics.crawler4j.url.WebURL;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -24,8 +22,11 @@ public class Crawler extends WebCrawler {
 	private final static String icsDomain = "http://www.ics.uci.edu";
 	private static final String subdomainFileDir = "../Assignment2";
 	private static final String subdomainFileName = "Subdomains.txt";
+	private static final String crawledDataDir = "../Assignment2/crawledData";
 	private static File subdomainFile;
 	private static ArrayList<String> urlCollection;
+	private static ArrayList<String> blackList;
+	private static int idCounter = 1;
 
 	/**
 	 * This methods performs a crawl starting at the specified seed URL. Returns a
@@ -34,11 +35,14 @@ public class Crawler extends WebCrawler {
 	 * @param seedURL - Seed URL to crawl
 	 */
 	public static Collection<String> crawl(String seedURL) {
-		Controller controller = new Controller(seedURL);
+		blackList = new ArrayList<String>();
+		blackList.add("duttgroup.ics.uci.edu/");
+		blackList.add("calendar.ics.uci.edu/");
+
 		urlCollection = new ArrayList<String>();
 		createSubdomainFile();
 
-		controller.start();
+		Controller.start(seedURL);
 
 		return urlCollection;
 
@@ -60,6 +64,12 @@ public class Crawler extends WebCrawler {
 		PageFetcher pageFetcher = new PageFetcher(config);
 		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
 		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+
+		// DO NOT visit URLs that are blacklisted
+		if(blackList.contains(url.toString())){
+			return false;
+
+		}
 
 		// If the robots.txt configuration is enabled and DOES NOT allow the given url
 		// DO NOT visit it
@@ -89,7 +99,6 @@ public class Crawler extends WebCrawler {
 			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
 			String text = htmlParseData.getText();
 			String html = htmlParseData.getHtml();
-			getWordInfo(url, text);
 			Set<WebURL> links = htmlParseData.getOutgoingUrls();
 
 			System.out.println("Text length: " + text.length());
@@ -97,9 +106,11 @@ public class Crawler extends WebCrawler {
 			System.out.println("Number of outgoing links: " + links.size());
 
 			if(!url.equals(icsDomain + "/")) {
-				addSubdomain(url, links.size());
+				addSubdomainToFile(url, links.size());
 
 			}
+
+			savePageDataToDatabase(htmlParseData);
 
 		}
 
@@ -116,6 +127,8 @@ public class Crawler extends WebCrawler {
 	///////////////////////////////////////////
 	public static void main(String[] args){
 		crawl(icsDomain);
+		sortSubdomainFile();
+		Controller.stop();
 
 	}
 
@@ -146,7 +159,6 @@ public class Crawler extends WebCrawler {
 		}
 
 	}
-
 	public static ArrayList<String> tokenizeString(String input){
 			ArrayList<String> toReturn = new ArrayList<String>();
 			try {
@@ -160,25 +172,28 @@ public class Crawler extends WebCrawler {
 			return toReturn;
 	}
 
-	private static HashMap<String, int> countFrequencies(String input) {
+	private static HashMap<String, Integer> countFrequencies(String input) {
 		ArrayList<String> toReturn = tokenizeString(input);
-		HashMap<String, int> toReturn;
+		HashMap<String, Integer> toReturn = new HashMap<>();
 		for (String toInsert : tokens) {
 			if (toReturn.containsKey(toInsert))
-				toReturn[toInsert] += 1;
+				toReturn.get(toInsert) += 1;
 			else
-				toReturn[toInsert] = 1;
+				toReturn.get(toInsert) = 1;
 		}
 	}
 
 	private static void getWordInfo(String url, String urlText) {
 		HashMap<String, int> wordFrequencies = countFrequencies(urlText);
-		String urlString = url.replaceAll("[^A-Za-z0-9 ]", "")
+		String urlString = url.replaceAll("[^A-Za-z0-9 ]", "");
 		int wordCount = 0;
+		BufferedWriter writer = null;
+
 		try	{
 			writer = new BufferedWriter(new FileWriter(url + "TEXT", true));
-			write.write(url);
+			writer.write(url);
 			writer.newLine();
+			for(Map.Entry<String, Integer> frequency : wordFrequencies.entrySet()) {
 			for (Map.entry<string, int> frequency : wordFrequencies.entrySet()) {
 				wordCount += frequency.getValue();
 				writer.write(frequency.getKey() + ", " + frequency.getValue());
@@ -189,11 +204,11 @@ public class Crawler extends WebCrawler {
 
 		} finally {
 			writer.close();
+
 		}
 
 	}
-
-	private static void addSubdomain(String url, int nLinks){
+	private static void addSubdomainToFile(String url, int nLinks){
 		BufferedWriter writer = null;
 
 		try {
@@ -216,5 +231,115 @@ public class Crawler extends WebCrawler {
 		}
 
 	}
+
+	/**
+	 * Code referenced for sorting items in a file:
+	 * http://www.avajava.com/tutorials/lessons/how-do-i-alphabetically-sort-the-lines-of-a-file.html%3Bjsess..
+	 */
+	private static void sortSubdomainFile(){
+		FileReader fileReader = null;
+		ArrayList<String> subdomainList = new ArrayList<String>();
+
+		try {
+			fileReader = new FileReader(subdomainFileName);
+
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				subdomainList.add(line);
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				fileReader.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+
+			}
+
+		}
+
+		Collections.sort(subdomainList, SubdomainComparator);
+
+		createSubdomainFile();
+
+		BufferedWriter writer = null;
+
+		try {
+			writer = new BufferedWriter(new FileWriter(subdomainFileName, true));
+			for(String subdomain : subdomainList) {
+				writer.write(subdomain);
+				writer.newLine();
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				writer.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			}
+
+		}
+
+	}
+	private static String generateUniqueId(){
+		UUID id = UUID.randomUUID();
+
+		return id.toString();
+
+	}
+	private static void savePageDataToDatabase(HtmlParseData parseData){
+		String text = parseData.getText();
+		String html = parseData.getHtml();
+
+		File pageFile = new File(crawledDataDir, String.valueOf(idCounter++));
+		BufferedWriter writer = null;
+
+		try {
+			writer = new BufferedWriter(new FileWriter(pageFile));
+			writer.write(html);
+			writer.newLine();
+			writer.write(text);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				writer.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			}
+
+		}
+
+	}
+
+
+	///////////////////////////////////////////
+	// SUBDOMAIN COMPARATOR
+	///////////////////////////////////////////
+	private static Comparator<String> SubdomainComparator = new Comparator<String>() {
+		@Override
+		public int compare(String sd1, String sd2) {
+			return sd1.toLowerCase().compareTo(sd2.toLowerCase());
+
+		}
+
+	};
 
 }
